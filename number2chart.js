8ffffd69c637c116372d71af4ab332baf02c7db7
keejun <keejunliu@gmail.com>
2014-01-04 14:47:04 +0800

    add funtion to jquery
    add funtion to jquery

--- number2chart.js
+++ number2chart.js
@@ -1,10 +1,7 @@
 ﻿/**
  * Created by keejun on 13-12-31.
  */
-var number2chart=new Object();
-    number2chart.chartbar=function(numberlist){
-        var regrex=/\{(\"?[\w|\d|\[\u4e00-\u9fa5\]]{1,}\"?\:\d{1,}\,?){1,}[^\W]\}/;
-        function obj2string(o){
+       function obj2string(o){
             var r=[];
             if(typeof o=="string"){
                 return "\""+o.replace(/([\'\"\\])/g,"\\$1").replace(/(\n)/g,"\\n").replace(/(\r)/g,"\\r").replace(/(\t)/g,"\\t")+"\"";
@@ -28,59 +25,7 @@ var number2chart=new Object();
             }
             return o.toString();
         }
-        var boolnumber=regrex.test(obj2string(numberlist));
-        if(!numberlist||typeof numberlist!="object"||boolnumber==false){
-            return false;
-        }
-        var numberpush = Object.keys(numberlist).map(function(key){
-            return numberlist[key];
-        });
-        var numberindex=Object.keys(numberlist).map(function(key){
-            return key;
-        })
-        var numbers=(numberpush).toString();//变量
-        //判断是否数字
-        var judgenumber=function(){
-            var number=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
-            var num=parseFloat(number);
-            if(!isNaN(num)){
-                return 1;
-            }
-            else{
-                return -1;
-            }
-        }
-        //过滤出数字数组
-        var arraynumber=function(){
-            var arrnumberfir=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,":");
-            var arrnumbersec=arrnumberfir.replace(/\:{1,}/g,":");
-            var arrnumberres=arrnumbersec.split(":");
-            if (arrnumberres[arrnumberres.length-1]==""){
-                arrnumberres.pop();
-            }
-            if(arrnumberres[0]==""){
-                arrnumberres=arrnumberres.slice(1);
-            }
-            return arrnumberres;
-        }
-        //对数组进行排序计算
-        var sumcount;
-        var countarry=function(arraynumber){
-            sumcount=arraynumber;
-            var percentarray={};
-            var sum =0;
-            for(var j=0;j<sumcount.length;j++){
-                sum=sum+parseInt(sumcount[j]);
-            }
-            for(var i=0;i<sumcount.length;i++){
-                percentarray["number"+i+":"+sumcount[i]]=(sumcount[i]/sum).toFixed(3);
-
-            }
-            // console.log(percentarray);
-            return percentarray;
-        }
-        //颜色库
-        var colorlib={
+       var colorlib={
             "mblue":"#1C86EE",
             "dgreen":"#32CD32",
             "dorange":"#DB570C",
@@ -97,15 +42,67 @@ var number2chart=new Object();
             "mgreenin":"#7CFC00",
             "dblack":"#545454",
             "bgray":"gray"
+        };
+       function getarraynumbers(numberlist){
+        var regrex=/\{(\"?[\w|\d|\[\u4e00-\u9fa5\]]{1,}\"?\:\d{1,}\,?){1,}[^\W]\}/;
+        var boolnumber=regrex.test(obj2string(numberlist));
+        if(!numberlist||typeof numberlist!="object"||boolnumber==false){
+            return false;
         }
-        /////////////////////////////////
+        var numberpush = Object.keys(numberlist).map(function(key){
+            return numberlist[key];
+        });
+        this.numberindex=Object.keys(numberlist).map(function(key){
+            return key;
+        })
+        var numbers=(numberpush).toString();//变量
+        //判断是否数字
+        this.judgenumber=function(){
+            var number=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
+            var num=parseFloat(number);
+            if(!isNaN(num)){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+        //过滤出数字数组
+        this.arraynumber=function(){
+            var arrnumberfir=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,":");
+            var arrnumbersec=arrnumberfir.replace(/\:{1,}/g,":");
+            var arrnumberres=arrnumbersec.split(":");
+            if (arrnumberres[arrnumberres.length-1]==""){
+                arrnumberres.pop();
+            }
+            if(arrnumberres[0]==""){
+                arrnumberres=arrnumberres.slice(1);
+            }
+            return arrnumberres;
+        }
+        //对数组进行排序计算
+        this.countarry=function(arraynumber){
+            this.sumcount=arraynumber;
+            var percentarray={};
+            var sum =0;
+            for(var j=0;j<this.sumcount.length;j++){
+                sum=sum+parseInt(this.sumcount[j]);
+            }
+            for(var i=0;i<this.sumcount.length;i++){
+                percentarray["number"+i+":"+this.sumcount[i]]=(this.sumcount[i]/sum).toFixed(3);
+            }
+            return percentarray;
+        }
+     }
+      // var number2chart=new Object();
+       var chartobar=function(numberlist,displaycanvas){
+         var funto= new getarraynumbers(numberlist);
         //创建canvas
-        var canvas=document.createElement("canvas");
+         var canvas=document.createElement("canvas");
         canvas.height=320;
         canvas.width=520;
         var circlex=canvas.width/10;
         var circley=canvas.height*5/6;
-        var displaycanvas=document.getElementById("displaybar");
         if(displaycanvas.hasChildNodes()){
             displaycanvas.innerHTML="";
         }
@@ -137,7 +134,7 @@ var number2chart=new Object();
         var inum=0;
         var drawtrangle=function(trangle){
             context.beginPath();
-            context.fillText(sumcount[inum],circlex+fontx,circley*(1-trangle)-10);
+            context.fillText(funto.sumcount[inum],circlex+fontx,circley*(1-trangle)-10);
             context.fillRect(circlex+fontx,circley*(1-trangle),25,circley*trangle);
             context.fillStyle=colors[colorindex];
             context.fill();
@@ -154,15 +151,16 @@ var number2chart=new Object();
                 var intextpercent=(trangle[index]*100).toFixed(1);
                 context.font="12px Microsoft YaHei";
                 context.fillStyle=colors[colorindex];
-                context.fillText(numberindex[li],circlex+fonty,circley+20);
+                context.fillText(funto.numberindex[li],circlex+fonty,circley+20);
                 context.fillText(intextpercent+"%",circlex+fonty,circley+40);
                 fonty=fonty+50;
                 li=li+1;
                 drawtrangle(trangle[index]);
             }
         }
-        if(judgenumber()){
-            var percentdict=countarry(arraynumber());
+
+        if(funto.judgenumber()){
+            var percentdict=funto.countarry(funto.arraynumber());
             drawxy();
             drawpercent(percentdict);
         }
@@ -170,110 +168,15 @@ var number2chart=new Object();
             return  "" ;
         }
     }
-//////////////////////////////////////////////////////////////////////////////
-    number2chart.chartpie=function (numberlist){
-    var regrex=/\{(\"?[\w|\d|\[\u4e00-\u9fa5\]]{1,}\"?\:\d{1,}\,?){1,}[^\W]\}/;
-    function obj2string(o){
-        var r=[];
-        if(typeof o=="string"){
-            return "\""+o.replace(/([\'\"\\])/g,"\\$1").replace(/(\n)/g,"\\n").replace(/(\r)/g,"\\r").replace(/(\t)/g,"\\t")+"\"";
-        }
-        if(typeof o=="object"){
-            if(!o.sort){
-                for(var i in o){
-                    r.push(i+":"+obj2string(o[i]));
-                }
-                if(!!document.all&&!/^\n?function\s*toString\(\)\s*\{\n?\s*\[native code\]\n?\s*\}\n?\s*$/.test(o.toString)){
-                    r.push("toString:"+o.toString.toString());
-                }
-                r="{"+r.join()+"}";
-            }else{
-                for(var i=0;i<o.length;i++){
-                    r.push(obj2string(o[i]))
-                }
-                r="["+r.join()+"]";
-            }
-            return r;
-        }
-        return o.toString();
-    }
-    var boolnumber=regrex.test(obj2string(numberlist));
-    if(!numberlist||typeof numberlist!="object"||boolnumber==false){
-        return false;
-    }
-    var numberpush = Object.keys(numberlist).map(function(key){
-        return numberlist[key];
-    });
-    var numberindex=Object.keys(numberlist).map(function(key){
-        return key;
-    })
-    var numbers=(numberpush).toString();//变量
-    //判断是否数字
-    var judgenumber=function(){
-        var number=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,"");
-        var num=parseFloat(number);
-        if(!isNaN(num)){
-            return 1;
-        }
-        else{
-            return -1;
-        }
-    }
-    //过滤出数字数组
-    var arraynumber=function(){
-        var arrnumberfir=numbers.replace(/[\ |\，|\[a-zA-Z\]+|\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\<|\.|\>|\/|\?]/g,":");
-        var arrnumbersec=arrnumberfir.replace(/\:{1,}/g,":");
-        var arrnumberres=arrnumbersec.split(":");
-        if (arrnumberres[arrnumberres.length-1]==""){
-            arrnumberres.pop();
-        }
-        if(arrnumberres[0]==""){
-            arrnumberres=arrnumberres.slice(1);
-        }
-        return arrnumberres;
-    }
-    //对数组进行排序计算
-    var sumcount;
-    var countarry=function(arraynumber){
-         sumcount=arraynumber;
-        var percentarray={};
-        var sum =0;
-        for(var j=0;j<sumcount.length;j++){
-            sum=sum+parseInt(sumcount[j]);
-        }
-        for(var i=0;i<sumcount.length;i++){
-            percentarray["number"+i+":"+sumcount[i]]=(sumcount[i]/sum).toFixed(3);
-
-        }
-
-        return percentarray;
-    }
-    //颜色库
-    var colorlib={
-        "mblue":"#1C86EE",
-        "dgreen":"#32CD32",
-        "dorange":"#DB570C",
-        "sblue":"#54FF9F",
-        "dpurple":"#9400D3",
-        "yellow":"#FFA500",
-        "scyan":"#00F5FF",
-        "lred":"#86173B",
-        "mgreen":"#90EE90",
-        "lblue":"#AEEEEE",
-        "lgreen":"#7FC712",
-        "lpurple":"#FF00FF",
-        "lblound":"#CDAD00",
-        "mgreenin":"#7CFC00",
-        "dblack":"#545454",
-        "bgray":"gray"
-    }
-    //创建canvas
-    var canvas=document.createElement("canvas");
-    canvas.height=320;
-    canvas.width=420;
-    var circlex=canvas.width/1.5;
-    var circley=canvas.height/2;
-    var displaycanvas=document.getElementById("displaypie");
+    var chartopie=function (numberlist,displaycanvas){
+       var funto= new getarraynumbers(numberlist);
+      //创建canvas
+       var canvas=document.createElement("canvas");
+        canvas.height=320;
+        canvas.width=420;
+        var circlex=canvas.width/1.5;
+        var circley=canvas.height/2;
+        console.log(displaycanvas);
     if(displaycanvas.hasChildNodes()){
         displaycanvas.innerHTML="";
     }
@@ -317,15 +220,14 @@ var number2chart=new Object();
             context.font="12px Microsoft YaHei";
             context.fillStyle=colors[colorindex];
             var fontpencent=parseFloat(trangle[index]).toFixed(5);
-
-            context.fillText(numberindex[li]+":"+parseFloat(fontpencent*100).toFixed(1)+"%("+sumcount[li]+")",circlex/16,circlex/3+fonty);
+            context.fillText(funto.numberindex[li]+":"+parseFloat(fontpencent*100).toFixed(1)+"%("+funto.sumcount[li]+")",circlex/16,circlex/3+fonty);
             fonty=fonty+25;
             li=li+1;
             drawtrangle(trangle[index]);
         }
     }
-    if(judgenumber()){
-        var percentdict=countarry(arraynumber());
+    if(funto.judgenumber()){
+        var percentdict=funto.countarry(funto.arraynumber());
         drawcircle();
         drawpercent(percentdict);
     }
@@ -333,5 +235,42 @@ var number2chart=new Object();
         return  "" ;
     }
 } ;
+String.prototype.trim=function(){
+    return this.replace(/(^\s*)|(\s*$)/g, "");
+}
+$.fn.extend({
+      chartbar:function(numberlist){
+          var select=this.selector.trim().toString();
+          var selectidbar=this.selector.trim().toString().split("");
+          if(selectidbar[0]=="#"){
+              var selected=document.getElementById(select.slice(1));
+              if(selected!=null){
+                  chartobar(numberlist,selected);
+              }
+              else{
+                  return "";
+              }
+           }
+         else{
+            return confirm("请使用id获取元素");
+           }
+         },
+      chartpie:function(numberlist){
+          var select=this.selector.trim().toString();
+          var selectidpie=this.selector.trim().toString().split("");
+          if(selectidpie[0]=="#"){
+              var selected=document.getElementById(select.slice(1));
+              if(selected!=null){
+                  chartopie(numberlist,selected);
+              }
+              else{
+                   return "";
+              }
+          }
+          else{
+               return confirm("请使用id获取元素");
+            }
+      }
+})
 
 
